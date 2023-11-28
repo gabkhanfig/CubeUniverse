92ac6ddc8df66fd836d157db2d41099a4cdf597c
gabkhanfig <gabkhanfig@gmail.com>
2023-11-28 01:23:12 -0500

    Define Numerical Types

---
+++ src/core.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <gk_types_lib/basic_types.h>
+
+using i8 = gk::i8;
+using i16 = gk::i16;
+using i32 = gk::i32;
+using i64 = gk::i64;
+using u8 = gk::u8;
+using u16 = gk::u16;
+using u32 = gk::u32;
+using u64 = gk::u64;
+using usize = gk::usize;
