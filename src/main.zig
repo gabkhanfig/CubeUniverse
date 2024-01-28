acd989d852245c12f730a56cc801a5692f46ca97
gabkhanfig <gabkhanfig@gmail.com>
2024-01-28 04:29:37 -0500

    Refactor For Swapchain

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 const c = @import("clibs.zig");
-const VulkanEngine = @import("shared/engine/vulkan/vk_engine.zig").VulkanEngine;
+//const VulkanEngine = @import("shared/engine/vulkan/vk_engine.zig").VulkanEngine;
+const VulkanEngine = @import("shared/engine/vulkan/VulkanEngine.zig");
 
 pub fn main() !void {
-    VulkanEngine.init();
-    defer VulkanEngine.deinit();
+    //VulkanEngine.init();
+    //defer VulkanEngine.deinit();
+    var e = VulkanEngine.init(std.heap.page_allocator);
+    e.cleanup();
 }
