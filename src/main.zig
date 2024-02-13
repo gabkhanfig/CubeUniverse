68f830b04ed4c79a69c346c3d15a1e47f9bcb7e7
gabkhanfig <gabkhanfig@gmail.com>
2024-02-13 15:08:07 -0500

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -13,45 +13,6 @@ const BlockPosition = world_transform.BlockPosition;
 const Application = @import("shared/Application.zig");
 
 pub fn main() !void {
-    //std.debug.print("\n", .{});
-    //VulkanEngine.init();
-    //defer VulkanEngine.deinit();
-
-    //var e = VulkanEngine.init(std.heap.page_allocator);
-    //e.cleanup();
-
-    // var window: *c.GLFWwindow = undefined;
-
-    // // Initialize the library
-    // if (c.glfwInit() == c.GLFW_FALSE) {
-    //     @panic("failed to init glfw");
-    // }
-
-    // // Create a windowed mode window and its OpenGL context */
-    // const createdWindow = c.glfwCreateWindow(640, 480, "Hello World", null, null);
-    // if (createdWindow == null) {
-    //     c.glfwTerminate();
-    //     @panic("failed to create glfw window");
-    // }
-
-    // window = createdWindow.?;
-
-    // var extensionCount: u32 = undefined;
-    // _ = c.vkEnumerateInstanceExtensionProperties(null, &extensionCount, null);
-
-    // std.debug.print("{} extensions supported\n", .{extensionCount});
-
-    // // Make the window's context current
-    // c.glfwMakeContextCurrent(window);
-
-    // // Loop until the user closes the window
-    // while (c.glfwWindowShouldClose(window) == c.GLFW_FALSE) {
-    //     // Poll for and process events
-    //     c.glfwPollEvents();
-    // }
-
-    // c.glfwTerminate();
-
     var app = Application.init(std.heap.page_allocator);
     defer app.deinit();
 
