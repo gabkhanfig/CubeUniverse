3fe2768734c0a61a70ae40aef74ede19f78cdaa5
gabkhanfig <gabkhanfig@gmail.com>
2024-02-17 16:31:03 -0500

    Create Swapchain

--- src/engine/Application.zig
+++ src/engine/Application.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
+const c = @import("clibs.zig");
+const Allocator = std.mem.Allocator;
 const Window = @import("graphics/Window.zig");
 const Pipeline = @import("graphics/vulkan/Pipeline.zig");
 const Device = @import("graphics/vulkan/device.zig").Device;
-const Allocator = std.mem.Allocator;
+const SwapChain = @import("graphics/vulkan/SwapChain.zig");
 
 const WINDOW_WIDTH = 960;
 const WINDOW_HEIGHT = 540;
@@ -13,19 +16,28 @@ allocator: Allocator,
 window: Window,
 device: Device,
 pipeline: Pipeline,
+swapChain: *SwapChain,
+pipelineLayout: c.VkPipelineLayout,
+commandBuffers: ArrayList(c.VkCommandBuffer),
 
 pub fn init(allocator: Allocator) *Self {
     const newSelf = allocator.create(Self) catch unreachable;
     newSelf.allocator = allocator;
     newSelf.window = Window.init(WINDOW_WIDTH, WINDOW_HEIGHT);
-    newSelf.device = Device.init(allocator, &newSelf.window);
-    newSelf.pipeline = Pipeline.init(&newSelf.device, Pipeline.PipelineConfigInfo.default(WINDOW_WIDTH, WINDOW_HEIGHT));
+    newSelf.device = Device.init(newSelf.allocator, &newSelf.window);
+    newSelf.swapChain = SwapChain.init(newSelf.allocator, &newSelf.device, c.VkExtent2D{ .width = @intCast(newSelf.window.width), .height = @intCast(newSelf.window.height) });
+
+    newSelf.createPipelineLayout();
+    newSelf.createPipeline();
+    //newSelf.createCommandBuffers(); TODO this
+
     return newSelf;
 }
 
 pub fn deinit(self: *Self) void {
     self.window.deinit();
     self.pipeline.deinit();
+    self.swapChain.deinit();
     self.device.deinit();
 
     const allocator = self.allocator;
@@ -36,4 +48,32 @@ pub fn run(self: *Self) void {
     while (!self.window.shouldClose()) {
         Window.pollEvents();
     }
+}
+
+fn createPipelineLayout(self: *Self) void {
+    var pipelineLayoutInfo: c.VkPipelineLayoutCreateInfo = .{};
+    pipelineLayoutInfo.sType = c.VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount = 0;
+    pipelineLayoutInfo.pSetLayouts = null;
+    pipelineLayoutInfo.pushConstantRangeCount = 0;
+    pipelineLayoutInfo.pPushConstantRanges = null;
+    if (c.vkCreatePipelineLayout(self.device.device, &pipelineLayoutInfo, null, &self.pipelineLayout) != c.VK_SUCCESS) {
+        @panic("Failed to create pipeline layout!");
+    }
+    //std.debug.assert(self.pipelineLayout != null);
+}
+
+fn createPipeline(self: *Self) void {
+    var pipelineConfig = Pipeline.PipelineConfigInfo.default(self.swapChain.width(), self.swapChain.height());
+    pipelineConfig.renderPass = self.swapChain.renderPass;
+    pipelineConfig.pipelineLayout = self.pipelineLayout;
+    self.pipeline = Pipeline.init(&self.device, pipelineConfig);
+}
+
+fn createCommandBuffers(self: *Self) void {
+    _ = self;
+}
+
+fn drawFrame(self: *Self) void {
+    _ = self; // TODO draw frame
 }
