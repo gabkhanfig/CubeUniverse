48e6d90000da5c7968f26fc9ab1f577c8c8b07f5
gabkhanfig <gabkhanfig@gmail.com>
2024-02-29 23:32:43 -0500

    Update tests.zig

--- src/tests.zig
+++ src/tests.zig
@@ -3,11 +3,11 @@ comptime {
     _ = @import("engine/world/world_transform.zig");
     _ = @import("engine/world/chunk/Chunk.zig");
     _ = @import("engine/world/chunk/Inner.zig");
-    _ = @import("engine/world/n_tree/NTree.zig");
+    _ = @import("engine/world/fat_tree/FatTree.zig");
     _ = @import("engine/types/color.zig");
     _ = @import("engine/types/light.zig");
     _ = @import("engine/types/job_system.zig");
-    _ = @import("engine/world/n_tree/LoadedChunksHashMap.zig");
+    _ = @import("engine/world/fat_tree/LoadedChunksHashMap.zig");
     _ = @import("engine/world/chunk/BlockStateIndices.zig");
     _ = @import("engine/math/vector.zig");
     _ = @import("engine/math/detail/vector2.zig");
