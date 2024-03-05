c263d3b100dfd2d7f926529a7e009b4607ea55be
gabkhanfig <gabkhanfig@gmail.com>
2024-03-05 03:55:09 -0500

    Update Window To OpenGL

--- src/engine/graphics/Window.zig
+++ src/engine/graphics/Window.zig
@@ -5,21 +5,22 @@ const c = @import("../clibs.zig");
 const GLFWwindow = c.GLFWwindow;
 const GLFW_TRUE = c.GLFW_TRUE;
 const GLFW_FALSE = c.GLFW_FALSE;
+const JobThread = @import("../types/job_system.zig").JobThread;
+//const Vec2i = @import("../math/vector.zig").Vector2(i32);
 
-const WINDOW_NAME = "Vulkan Engine";
+const WINDOW_NAME = "Cube Universe";
 
 const Self = @This();
 
-window: *GLFWwindow,
+glfwwindow: *GLFWwindow,
 width: i32,
 height: i32,
 
-pub fn init(width: i32, height: i32) Self {
+pub fn init(renderThread: *JobThread, width: i32, height: i32) Self {
     if (c.glfwInit() == GLFW_FALSE) {
         @panic("failed to init glfw");
     }
 
-    c.glfwWindowHint(c.GLFW_CLIENT_API, c.GLFW_NO_API); // Dont use OpenGL
     c.glfwWindowHint(c.GLFW_RESIZABLE, GLFW_FALSE);
 
     const createdWindow = c.glfwCreateWindow(width, height, WINDOW_NAME, null, null);
@@ -30,16 +31,19 @@ pub fn init(width: i32, height: i32) Self {
 
     const window = createdWindow.?;
 
-    return Self{ .window = window, .width = width, .height = height };
+    const future = renderThread.runJob(c.glfwMakeContextCurrent, .{window}) catch unreachable;
+    future.wait();
+
+    return Self{ .glfwwindow = window, .width = width, .height = height };
 }
 
 pub fn deinit(self: Self) void {
-    c.glfwDestroyWindow(self.window);
+    c.glfwDestroyWindow(self.glfwwindow);
     c.glfwTerminate();
 }
 
 pub fn shouldClose(self: Self) bool {
-    const result = c.glfwWindowShouldClose(self.window);
+    const result = c.glfwWindowShouldClose(self.glfwwindow);
     return result == GLFW_TRUE;
 }
 
@@ -47,8 +51,17 @@ pub fn pollEvents() void {
     c.glfwPollEvents();
 }
 
-pub fn createSurface(self: *Self, instance: c.VkInstance, surface: *c.VkSurfaceKHR) void {
-    if (c.glfwCreateWindowSurface(instance, self.window, null, surface) != c.VK_SUCCESS) {
-        @panic("Failed to create window surface");
-    }
-}
+// TODO implement window switching and resizing later
+// pub const WindowDimensionsTag = enum {
+//     windowed,
+//     fullscreen,
+// };
+
+// pub const WindowDimensions = union(WindowDimensionsTag) {
+//     windowed: Vec2i,
+//     fullscreen: bool,
+// };
+
+// pub const WindowInitParams = struct {
+//     dimensions: WindowDimensions,
+// };
